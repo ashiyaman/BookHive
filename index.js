5b55a6bc5e6e84214e1386df6b5c67c42a5a10d7
ashiyaman <ashiya1005@gmail.com>
2025-03-06 01:50:12 +0530

    Fix errors

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.put('/books/:bookId', async (req, res) => {
   const bookId = req.params.bookId
   try{
     const updatedBook = await Books.findByIdAndUpdate(bookId, req.body, {new: true})
-    console.log(updatedBook)
+    console.log('...up....', updatedBook)
     if(!updatedBook){
       return res.status(404).json({message: 'Book not found'})
     }
